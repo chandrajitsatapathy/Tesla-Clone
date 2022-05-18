2feaf528616c9ced68bae16990bd3cf2b6a1ead4
Chandrajit Satapathy <chandrajitsatapathy9@gmail.com>
2022-05-18 20:07:34 +0530

    Updated all the dependancies

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Hi, I am Chandrajit Satapathy.
 This is my tesla-clone project using React.
+My GitHub: https://github.com/chandrajitsatapathy
+My LinkedIn: https://www.linkedin.com/in/chandrajit-satapathy-6a5b2396/
 Thanks.
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
     return (
         <Container>
             <a>
-                <img src="/images/logo.svg" alt=""/>
+                <img src="/images/logo.svg" alt="tesla-logo"/>
             </a>
             <Menu>
                 {cars && cars.map((car, index) => (
